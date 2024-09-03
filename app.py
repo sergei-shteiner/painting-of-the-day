05e8e4828d8769a9f1988031bef42588ac4251f7
Sergei Shteiner <sergei.shteiner@gmail.com>
2024-09-03 23:46:52 +0200

    get client date

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
 from flask import Flask, render_template
-from babel.dates import format_date
-from datetime import datetime
-import locale
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # User locale detection
-    user_locale = locale.getdefaultlocale()[0]
-    
-    # Getting the current date
-    today = datetime.now()
-    
-    # Formatting the date using Babel
-    formatted_date = format_date(today, locale=user_locale)
-    
-    return render_template('index.html', date=formatted_date)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
- 
