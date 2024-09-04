97fba71e13922e603fd9bc32df9fbaa15ffb8c11
Sergei Shteiner <sergei.shteiner@gmail.com>
2024-09-04 13:59:42 +0200

    use ip defined country, then browser defined

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+def get_client_country(ip):
+    try:
+        response = requests.get(f'http://ipinfo.io/{ip}/json')
+        data = response.json()
+        return data.get('country')
+    except Exception as e:
+        print(f"Error getting location: {e}")
+        return None
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    client_ip = request.remote_addr
+    client_country = get_client_country(client_ip)
+    return render_template('index.html', country=client_country)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
